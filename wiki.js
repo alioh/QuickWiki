a33258e6b250d73e9f4efd8844623bb793a42387
alioh <lo.ol.123@gmail.com>
2015-09-16 19:33:24 -0400

    title/links/image and better summary

--- wiki.js
+++ wiki.js
@@ -2,9 +2,12 @@
 if (Meteor.isClient) {
   Template.mainContent.helpers({
     summary: function () {
+      var summarycode = Session.get("summ");
+      var summ = summarycode.replace(/(<([^>]+)>)/ig,"");
       var data = {
         title: Session.get("title"),
-        summ: Session.get("summ") || "You didn't pick a topic yet!"
+        summ: summ,
+        imageurl: Session.get("imageurl")
       }
       return data;
     }
@@ -12,23 +15,28 @@ if (Meteor.isClient) {
 
   Template.mainInput.events({
     'click #topicButton': function (e) {
-      e.preventDefault();
-      var topic = $('#topicInput').val();
-      var language = "en"
-      Session.set("test", topic);
-      var summary = Meteor.call("wiki", topic, language);
-      Meteor.call('wiki', topic, function (error, result) {
-       Session.set("summ", result.summary);
-       Session.set("title", result.title);
-      });
-      $('#topicInput').val('');
+      if ((e.type === 'click') || (e.type === 'keyup' && e.which === 13) ) {
+        var topic = $('#topicInput').val();
+        var language = "en";
+        var summary = Meteor.call("wiki", topic, language);
+        Meteor.call('wiki', topic, function (error, result) {
+         Session.set("summ", result.summary);
+         Session.set("title", result.title);
+         Session.set("imageurl", result.image.url)
+        });
+        $('#topicInput').val('');
+      }
     }
   });
 }
 
 Meteor.methods({
     wiki: function(topic, language) {
+      if(language !== "en"){
         var wikiTest = Scrape.wikipedia(topic, language);
-        return wikiTest;
+      } else {
+        var wikiTest = Scrape.wikipedia(topic);
+      }
+      return wikiTest;
     },
 });
